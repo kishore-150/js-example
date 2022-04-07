415bdc913124e634e300247e8424e85d2d7e1762
kishore-150 <kishore.kr@fr8.in>
2022-04-07 22:41:10 +0530

    graphql changed fetch to graphclient

---
+++ sql/graphql_request.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const bodyparser=require('body-parser');
+const { gql,GraphQLClient} = require('graphql-request');
+
+app.use(bodyparser.json())
+  async function city(){
+    app.get('/city',async(req,res) => {
+    const query = gql`
+    query city($search: String) {
+    city(where: {_and: [{name: {_ilike: $search}}, {is_connected_city: {_eq: true}}]}) {
+    id
+    name
+    is_connected_city
+    }
+  }`
+const variables = {
+    search: `${req.query.name}%`
+}
+   const client= new GraphQLClient('https://dcore.fr8.in/v1/graphql',{
+    method:'POST',
+    headers:{'Content-type': 'application/json'},       
+    body : JSON.stringify({
+         query,
+         variables
+       }) 
+   })
+   
+  const data = await client.request(query,variables) 
+
+  res.json(data);
+}) 
+app.listen(4000,() => 
+    {console.log("server  is running")});
+}  
+  
+   city()
